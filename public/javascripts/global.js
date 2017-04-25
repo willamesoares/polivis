fab5a30770e2d88ca465a2df37bfac56c76ca819
Willame Soares <willame_soares@hotmail.com>
2017-04-25 01:30:45 -0300

    vega/setup: Add vega grammar and example

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,30 +1,150 @@
 $(document).ready(() => {
-  const svg = d3.select("svg"),
-      angles = d3.range(0, 2 * Math.PI, Math.PI / 200);
 
-  const path = svg.append("g")
-      .attr("transform", "translate(480, 250)")
-      .attr("fill", "none")
-      .attr("stroke-width", 10)
-      .attr("stroke-linejoin", "round")
-      .selectAll("path")
-      .data(["cyan", "magenta", "yellow"])
-      .enter().append("path")
-      .attr("stroke", function(d) { return d; })
-      .style("mix-blend-mode", "darken")
-      .datum(function(d, i) {
-        return d3.radialLine()
-            .curve(d3.curveLinearClosed)
-            .angle(function(a) { return a; })
-            .radius(function(a) {
-              var t = d3.now() / 1000;
-              return 200 + Math.cos(a * 8 - i * 2 * Math.PI / 3 + t) * Math.pow((1 + Math.cos(a - t)) / 2, 3) * 32;
-            });
-      });
+  let vegaJSONSpec = {
+    "$schema": "https://vega.github.io/schema/vega/v3.0.json",
+    "width": 700,
+    "height": 400,
+    "padding": 5,
 
-  d3.timer(function() {
-    path.attr("d", function(d) {
-      return d(angles);
-    });
-  });
+    "config": {
+      "axisY": {
+        "titleX": -2,
+        "titleY": 410,
+        "titleAngle": 0,
+        "titleAlign": "right",
+        "titleBaseline": "top"
+      }
+    },
+
+    "data": [
+      {
+        "name": "voters",
+        "url": "data/json/voter_profile.json"
+      },
+      {
+        "name": "fields",
+        "values": [
+          "NUM_ZONA",
+          "NUM_SECAO",
+          "ESTADO_CIVIL",
+          "FAIXA_ETARIA",
+          "GRAU_ESCOLARIDADE",
+          "SEXO",
+          "QTD_ELEITORES_NO_PERFIL"
+        ]
+      }
+    ],
+
+    "scales": [
+      {
+        "name": "ord", "type": "point",
+        "range": "width", "round": true,
+        "domain": {"data": "fields", "field": "data"}
+      },
+      {
+        "name": "NUM_ZONA", "type": "linear",
+        "range": "height", "zero": false, "nice": true,
+        "domain": {"data": "voters", "field": "NUM_ZONA"}
+      },
+      {
+        "name": "NUM_SECAO", "type": "linear",
+        "range": "height", "zero": false, "nice": true,
+        "domain": {"data": "voters", "field": "NUM_SECAO"}
+      },
+      {
+        "name": "ESTADO_CIVIL", "type": "linear",
+        "range": "height", "zero": false, "nice": true,
+        "domain": {"data": "voters", "field": "ESTADO_CIVIL"}
+      },
+      {
+        "name": "FAIXA_ETARIA", "type": "linear",
+        "range": "height", "zero": false, "nice": true,
+        "domain": {"data": "voters", "field": "FAIXA_ETARIA"}
+      },
+      {
+        "name": "GRAU_ESCOLARIDADE", "type": "linear",
+        "range": "height", "zero": false, "nice": true,
+        "domain": {"data": "voters", "field": "GRAU_ESCOLARIDADE"}
+      },
+      {
+        "name": "SEXO", "type": "linear",
+        "range": "height", "zero": false, "nice": true,
+        "domain": {"data": "voters", "field": "SEXO"}
+      },
+      {
+        "name": "QTD_ELEITORES_NO_PERFIL", "type": "linear",
+        "range": "height", "zero": false, "nice": true,
+        "domain": {"data": "voters", "field": "QTD_ELEITORES_NO_PERFIL"}
+      }
+    ],
+
+    "axes": [
+      {
+        "orient": "left", "zindex": 1,
+        "scale": "NUM_ZONA", "title": "Zona",
+        "offset": {"scale": "ord", "value": "NUM_ZONA", "mult": -1}
+      },
+      {
+        "orient": "left", "zindex": 1,
+        "scale": "NUM_SECAO", "title": "Secao",
+        "offset": {"scale": "ord", "value": "NUM_SECAO", "mult": -1}
+      },
+      {
+        "orient": "left", "zindex": 1,
+        "scale": "ESTADO_CIVIL", "title": "Estado Civil",
+        "offset": {"scale": "ord", "value": "ESTADO_CIVIL", "mult": -1}
+      },
+      {
+        "orient": "left", "zindex": 1,
+        "scale": "FAIXA_ETARIA", "title": "Faixa Etaria",
+        "offset": {"scale": "ord", "value": "FAIXA_ETARIA", "mult": -1}
+      },
+      {
+        "orient": "left", "zindex": 1,
+        "scale": "GRAU_ESCOLARIDADE", "title": "Grau Esc.",
+        "offset": {"scale": "ord", "value": "GRAU_ESCOLARIDADE", "mult": -1}
+      },
+      {
+        "orient": "left", "zindex": 1,
+        "scale": "SEXO", "title": "Sexo",
+        "offset": {"scale": "ord", "value": "SEXO", "mult": -1}
+      },
+      {
+        "orient": "left", "zindex": 1,
+        "scale": "QTD_ELEITORES_NO_PERFIL", "title": "Qtd.", "format": "d",
+        "offset": {"scale": "ord", "value": "QTD_ELEITORES_NO_PERFIL", "mult": -1}
+      }
+    ],
+
+    "marks": [
+      {
+        "type": "group",
+        "from": {"data": "voters"},
+        "marks": [
+          {
+            "type": "line",
+            "from": {"data": "fields"},
+            "encode": {
+              "enter": {
+                "x": {"scale": "ord", "field": "data"},
+                "y": {
+                  "scale": {"datum": "data"},
+                  "field": {"parent": {"datum": "data"}}
+                },
+                "stroke": {"value": "steelblue"},
+                "strokeWidth": {"value": 1.01},
+                "strokeOpacity": {"value": 0.3}
+              }
+            }
+          }
+        ]
+      }
+    ]
+  }
+
+  const view = new vega.View(vega.parse(vegaJSONSpec))
+      .renderer('svg')                             // set renderer (canvas or svg)
+      .initialize(document.querySelector('#view')) // initialize view within parent DOM container
+      .hover()                                     // enable hover encode set processing
+      .run();                                      // run the dataflow and render the view
 })
